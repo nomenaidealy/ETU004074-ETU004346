8e116001b46c2ef189eff47862e399ef9f34cf7e
nomenaidealy <idealyanjara2@gmail.com>
2025-07-14 12:25:40 +0300

    mdp

--- inc/connection.php
+++ inc/connection.php
@@ -4,7 +4,7 @@ function dbconnect()
     static $connect = null;
 
     if ($connect === null) {
-        $connect = mysqli_connect('localhost', 'root', '', 'projet1');
+        $connect = mysqli_connect('localhost', 'ETU004074', 'wthDMxuC', 'db_s2_ETU004074');
 
         if (!$connect) {
 
